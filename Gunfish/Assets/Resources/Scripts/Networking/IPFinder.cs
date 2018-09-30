4542fca9a1763fa6789c31537a708d740f78f904
Wyatt Joyner <wcj3te@virginia.edu>
2018-09-30 11:06:28 -0400

    Added IP Finder

---
+++ Gunfish/Assets/Resources/Scripts/Networking/IPFinder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class IPFinder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string hostName = System.Net.Dns.GetHostName();
+        string localIP = System.Net.Dns.GetHostEntry(hostName).AddressList[0].ToString();
+
+        GetComponent<NetworkManager>().networkAddress = localIP;
+    }
+}
