8f94835f6046023787741f8c15e442946c95dae1
Noah M <nm6ss@virginia.edu>
2018-10-04 18:05:33 -0400

    Basic Main Menu

---
+++ Gunfish/Assets/Resources/Scripts/UI/CustomNetworkManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager {
+    //A temporary override for Network Manager to expose some important features that UI doesn't normally have access to.
+
+
+
+    /// <summary>
+    /// A wrapper for NetworkManager's StartHost method. 
+    /// </summary>
+    public void StartHost_Button()
+    {
+        base.StartHost();
+    }
+
+    public void StartClient_Button()
+    {
+        base.StartClient();
+    }
+
+    public void UpdateAddress()
+    {
+        networkAddress = FindMainMenu().GetComponent<MainMenuManager>().Addr;
+    }
+
+    public void UpdatePort()
+    {
+        string port = FindMainMenu().GetComponent<MainMenuManager>().Port;
+        int res;
+        bool success = int.TryParse(port, out res);
+        if(success)
+            networkPort = res;
+        else
+        {
+            Debug.Log("Failed to set port");
+        }
+    }
+
+    public GameObject FindMainMenu()
+    {
+        return GameObject.Find("mainmenu");
+    }
+
+}
---
+++ Gunfish/Assets/Resources/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour {
+    private Text addrString;
+    private Text portString;
+
+    Button joinButton;
+    Button hostButton;
+
+    public string Addr
+    {
+        get {
+            if (addrString != null)
+                return addrString.text;
+            else return null;
+        }
+    }
+
+    public string Port
+    {
+        get
+        {
+            if (portString != null)
+                return portString.text;
+            else return null;
+        }
+    }
+
+    void Awake()
+    {
+        //Goes Panel -> JoinContainer -> (1: addr, 2:port) -> Text component
+        addrString = transform.GetChild(2).GetChild(1).GetChild(1).GetComponent<Text>();
+        portString = transform.GetChild(2).GetChild(2).GetChild(1).GetComponent<Text>();
+        hostButton = transform.GetChild(1).GetComponent<Button>(); //Get buttons for method delegation
+        joinButton = transform.GetChild(2).GetChild(0).GetComponent<Button>();
+
+        CustomNetworkManager manager = GameObject.Find("NetworkManager").GetComponent<CustomNetworkManager>();
+
+        hostButton.onClick.AddListener(delegate { manager.StartHost_Button(); });
+        joinButton.onClick.AddListener(delegate { manager.StartClient_Button(); });
+
+    }
+
+    
+
+
+}
