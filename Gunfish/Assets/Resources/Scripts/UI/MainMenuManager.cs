01c6350ee45b188d6959c2164ca315504d0d5b19
Noah M <nm6ss@virginia.edu>
2018-10-14 19:33:55 -0400

    Settings and Quit

--- Gunfish/Assets/Resources/Scripts/UI/MainMenuManager.cs
+++ Gunfish/Assets/Resources/Scripts/UI/MainMenuManager.cs
@@ -7,10 +7,15 @@ public class MainMenuManager : MonoBehaviour {
     private Text addrString;
     private Text portString;
 
+    private bool settingsOpen = false;
+
     Button joinButton;
     Button hostButton;
     InputField addrField;
     InputField portField;
+
+    Transform mainMenu;
+    Transform settingsMenu;
 
     public string Addr
     {
@@ -33,13 +38,16 @@ public class MainMenuManager : MonoBehaviour {
 
     void Awake()
     {
+        mainMenu = transform.GetChild(0);
+        settingsMenu = transform.GetChild(1);
+
         //Goes Panel -> JoinContainer -> (1: addr, 2:port) -> Text component
-        addrField = transform.GetChild(2).GetChild(1).GetComponent<InputField>();
-        portField = transform.GetChild(2).GetChild(2).GetComponent<InputField>();
-        hostButton = transform.GetChild(1).GetComponent<Button>(); //Get buttons for method delegation
-        joinButton = transform.GetChild(2).GetChild(0).GetComponent<Button>();
-        addrString = transform.GetChild(2).GetChild(1).GetChild(1).GetComponent<Text>();
-        portString = transform.GetChild(2).GetChild(2).GetChild(1).GetComponent<Text>();
+        addrField = mainMenu.GetChild(2).GetChild(1).GetComponent<InputField>();
+        portField = mainMenu.GetChild(2).GetChild(2).GetComponent<InputField>();
+        hostButton = mainMenu.GetChild(1).GetComponent<Button>(); //Get buttons for method delegation
+        joinButton = mainMenu.GetChild(2).GetChild(0).GetComponent<Button>();
+        addrString = mainMenu.GetChild(2).GetChild(1).GetChild(1).GetComponent<Text>();
+        portString = mainMenu.GetChild(2).GetChild(2).GetChild(1).GetComponent<Text>();
 
         CustomNetworkManager manager = GameObject.Find("NetworkManager").GetComponent<CustomNetworkManager>();
 
@@ -48,6 +56,31 @@ public class MainMenuManager : MonoBehaviour {
         addrField.onEndEdit.AddListener(delegate { manager.UpdateAddress(); });
         portField.onEndEdit.AddListener(delegate { manager.UpdatePort(); });
 
+    }
+
+    /// <summary>
+    /// Call to quit the game.
+    /// </summary>
+    public void ExitGame()
+    {
+        Debug.Log("We'd quit if we weren't in the editor right now...");
+        Application.Quit();
+    }
+
+    public void ToggleSettings()
+    {
+        if(settingsOpen)
+        {
+            settingsMenu.gameObject.SetActive(false);
+            mainMenu.gameObject.SetActive(true);
+            settingsOpen = false;
+        }
+        else
+        {
+            settingsMenu.gameObject.SetActive(true);
+            mainMenu.gameObject.SetActive(false);
+            settingsOpen = true;
+        }
     }
 
     
