6fc254046276aa2cc2836f4b7361501e94679069
AdamE123 <AdamElSheikh99@gmail.com>
2018-10-14 19:02:58 -0400

    PlayerManager
    Basically a skeleton for the player manager at this point

---
+++ Gunfish/Assets/PlayerManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public static PlayerManager PM;
+    public List<GameObject> playerList;
+
+    // Use this for initialization
+    void Start () {
+        // Initialize this class as a singleton
+        if (PM == null)
+            PM = this;
+        else
+            Destroy(this);
+
+        // Initialize a list of players
+        playerList = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Function to add a new player to the list
+    void AddPlayer(GameObject newPlayer) {
+        playerList.Add(newPlayer);
+    }
+}
