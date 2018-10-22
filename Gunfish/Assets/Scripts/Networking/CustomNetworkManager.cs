cdae079d1e25e3e210aa34119066bc4b9102926a
JustinBecker26 <jab7yp@virginia.edu>
2018-10-21 20:10:10 -0400

    SpawnRoom(NOT WORKING WHY NOT RYAN HEEEEEELP)

--- Gunfish/Assets/Scripts/Networking/CustomNetworkManager.cs
+++ Gunfish/Assets/Scripts/Networking/CustomNetworkManager.cs
@@ -5,13 +5,16 @@ using System.Collections.Generic;
 public class CustomNetworkManager : NetworkManager  
 {
     public List<GameObject> fishList;
+    private NetworkStartPosition[] spawnPoints;
+
     public override void OnStartServer()
     {
         fishList = new List<GameObject>(GunfishList.Get());
+        spawnPoints = FindObjectsOfType<NetworkStartPosition>();
     }
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
-        GameObject player = (GameObject)Instantiate(fishList[Random.Range(0,fishList.Count)], Vector3.zero, Quaternion.identity);
+        GameObject player = (GameObject)Instantiate(fishList[Random.Range(0,fishList.Count)], spawnPoints[0].transform.position, Quaternion.identity);
         NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
     }
 }
