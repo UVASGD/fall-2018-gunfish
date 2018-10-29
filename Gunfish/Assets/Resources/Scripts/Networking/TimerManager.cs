2373a36e0f5594534cc672b928493ca02f89c87c
Noah M <nm6ss@virginia.edu>
2018-10-28 20:09:12 -0400

    TimerManager Skeleton

---
+++ Gunfish/Assets/Resources/Scripts/Networking/TimerManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TimerManager : NetworkBehaviour {
+
+    [SyncVar]
+    float gameTime; //Game length
+
+    public float CurTime
+    {
+        get { return gameTime; }
+    }
+
+    [SyncVar]
+    int state = 0; //0 = Waiting for Ready. 1 = Game running. 2 = Game is done
+
+    [SyncVar]
+    int playerCount; //How many players are currently in the game.
+
+    static TimerManager server;
+    public static TimerManager ServerTimer
+    {
+        get { return server; }
+    }
+
+    void Start()
+    {
+        if(isServer)
+        {
+            if(isLocalPlayer) //We are the server
+            {
+                server = this;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(isServer) //Server time update
+        {
+            switch (state)
+            {
+                case 0: //Waiting on players to be ready
+                    if (false) //This should check if everyone is ready
+                    {
+                        state = 1;
+                        gameTime = 0;
+                    }
+                    break;
+
+                case 1: //Running as normal
+                    gameTime += Time.deltaTime;
+                    break;
+
+                case 2: //Game end
+                    //Level has finished
+                    break;
+            }
+            
+        }
+   
+    }
+
+
+
+}
