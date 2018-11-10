a9264390c699deb460caad5800b21d2aefd6514e
Julian <jaykmcclinton@gmail.com>
2018-11-10 00:13:49 -0500

    in alabama

--- Gunfish/Assets/Scripts/Clam.cs
+++ Gunfish/Assets/Scripts/Clam.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Clam : MonoBehaviour
 {
     bool isClammed;
+    private Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
 
         if (collider.CompareTag("Gunfish") && !isClammed)
         {
             GetComponent<FixedJoint2D>().connectedBody = collider.gameObject.GetComponent<Rigidbody2D>();
+            anim.SetTrigger("Toggle");
             isClammed = true;
             StartCoroutine(clamRelease());
         }
@@ -20,6 +28,7 @@ public class Clam : MonoBehaviour
     {
         yield return new WaitForSeconds(2);
         GetComponent<FixedJoint2D>().connectedBody = null;
+        anim.SetTrigger("Toggle");
         StartCoroutine(clamWait());
     }
 
