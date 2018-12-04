36273a9f114679c7ef6f3373ff80d4230404d0b5
Unknown <ryanakann@gmail.com>
2018-12-04 11:17:34 -0500

    Broken Clam Circle

---
+++ Gunfish/Assets/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float degreesPerSecond = 90f;
+    public bool allowRotation = false;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward, degreesPerSecond * Time.deltaTime);
+
+        if (!allowRotation) {
+            foreach (Transform child in transform) {
+                child.rotation = Quaternion.identity;
+            }
+        }
+	}
+}
