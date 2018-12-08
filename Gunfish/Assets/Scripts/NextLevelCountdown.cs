e6cdd915ef1ed4ae72f6010d0447217aea746b20
Unknown <ryanakann@gmail.com>
2018-12-08 00:57:41 -0500

    In Game Timers Initialized

--- Gunfish/Assets/Scripts/NextLevelCountdown.cs
+++ Gunfish/Assets/Scripts/NextLevelCountdown.cs
@@ -6,17 +6,40 @@ using UnityEngine.Networking;
 
 public class NextLevelCountdown : NetworkBehaviour {
 
+    public static NextLevelCountdown instance;
+
+    static bool registered;
+
     public Text text;
     private int secondsRemaining = 10;
 
 	// Use this for initialization
 	void Awake () {
-        NetworkManager.singleton.client.RegisterHandler(MessageTypes.REQUESTTIME, OnRequestTime);
+        if (!registered) {
+            NetworkManager.singleton.client.RegisterHandler(MessageTypes.REQUESTTIME, OnRequestTime);
+            registered = true;
+        }
+    }
+
+    private void Start () {
+        //if (!instance) {
+        //    instance = this;
+        //} else {
+        //    Destroy(this);
+        //}
+
+        //DontDestroyOnLoad(this);
     }
 
     [ClientCallback]
     void OnRequestTime (NetworkMessage netMsg) {
-        if (!text) return;
+        if (!text) {
+            if (!GameObject.FindWithTag("Timer").GetComponent<Text>()) {
+                return;
+            } else {
+                text = GameObject.FindWithTag("Timer").GetComponent<Text>();
+            }
+        }
 
         RequestTimeMsg msg = netMsg.ReadMessage<RequestTimeMsg>();
         secondsRemaining = msg.time;
